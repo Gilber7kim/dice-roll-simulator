376fde66eb4f9d4a16ad2934d6d9093aa8276290
Gilbert Kim <gilbertkim@seoulchristian.org>
2016-01-24 20:34:37 +0900

    Fixed dice project

--- dice-roll-simulator.py
+++ dice-roll-simulator.py
@@ -1,40 +1,40 @@
-#This makes the while loop that makes the program run forever.
+# This makes the while loop that makes the program run forever.
 while True:
     # it imports the module random.
     import random
-    # These two commands print "Hi!! Welcome to Gilbert's ramdom dicedice simulator!" and "Press ctrl+c to quit"
+    # These two commands print what I typed in the quotation marks
     print("Hi!! Welcome to Gilbert's ramdom dicedice simulator!")
     print("Press ctrl+c to quit")
-    # Command raw_input allows you to type something. In this case, it allows you to type enter or ctrl+C by pausing the while loop.
+    # Command raw_input allows you to type something in.
     raw_input("Enter to continue")
     # It makes the numbers between one to six to show up in the dice randomly.
-    number = random.randint (1,6)
+    number = random.randint(1, 6)
     # These are the possibilities of dice
-    if number==1:
+    if number == 1:
         print ("[----------------]")
         print ("[----------------]")
         print ("[-------()-------]")
         print ("[----------------]")
         print ("[----------------]")
-    if number==2:
+    if number == 2:
         print ("[----------------]")
         print ("[----------------]")
         print ("[--()--------()--]")
         print ("[----------------]")
         print ("[----------------]")
-    if number==3:
+    if number == 3:
         print ("[-()-------------]")
         print ("[----------------]")
         print ("[-------()-------]")
         print ("[----------------]")
         print ("[-------------()-]")
-    if number==4:
+    if number == 4:
         print ("[-()----------()-]")
         print ("[----------------]")
         print ("[----------------]")
         print ("[----------------]")
         print ("[-()----------()-]")
-    if number==5:
+    if number == 5:
         print ("[-()----------()-]")
         print ("[----------------]")
         print ("[-------()-------]")
